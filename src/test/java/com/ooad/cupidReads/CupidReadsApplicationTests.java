f260a397c4d85ec0f8b4a8a15fbc364780dbf4f6
SamDadmi <sudha.dadmi22@gmail.com>
2025-03-26 13:39:40 +0530

    home page beautified

---
+++ src/test/java/com/ooad/cupidReads/CupidReadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ooad.cupidReads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupidReadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
